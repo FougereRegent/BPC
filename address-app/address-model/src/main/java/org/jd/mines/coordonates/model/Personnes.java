04ea8fdfbb077007f257c749a108eb4bbdd7da28
damien venant-valéry <damien.venant@outlook.com>
2023-03-15 14:16:19 +0100

    Ajout de la class Personnes

--- address-app/address-model/src/main/java/org/jd/mines/coordonates/model/Personnes.java
+++ address-app/address-model/src/main/java/org/jd/mines/coordonates/model/Personnes.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 
 import java.util.Set;
 import java.util.UUID;
-import lombok.Data;
 
 import java.lang.*;
 import java.util.*;
@@ -13,11 +12,66 @@ import java.util.*;
 @Table(name = "Personnes")
 public class Personnes {
 
-	@Id
 	private long id;
 	private String nom;
 	private String prenom;
 	private Date naissanceDate;
 	private String telNumero;
 	private String mail;
+
+
+	@Id
+	@Column(name = "id")
+	public long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "nom")
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	@Column(name = "Prenom")
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	@Column(name ="dateniassance")
+	public Date getDate() {
+		return naissanceDate;
+	}
+
+	public void setDate(Date naissanceDate) {
+		this.naissanceDate = naissanceDate;
+	}
+
+	@Column(name = "numerotelephone")
+	public String getTelNum() {
+		return telNumero;
+	}
+
+	public void setTelNum(String telNumero) {
+		this.telNumero = telNumero;
+	}
+
+	@Column(name = "mail")
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
 }
